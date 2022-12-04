146c762f4ac2ffdff72a3c49e8fa175b4dc30642
sabriyeuregen <sabriyeuregen@gmail.com>
2022-12-04 22:43:00 +0300

    Updated : Home.scss

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import "./Home.scss";
 import homeimage from "../../assets/homeimage.jpg";
-import homeimgtwo from "../../assets/home-imgtwo.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 const Home = () => {
   return (
     <div className="home">
       <img className="home__img" src={homeimage} alt="home"></img>
-      <img className="home__img-two" src={homeimgtwo} alt="imgtwo"></img>
+      {/*<img className="home__img-two" src={homeimgtwo} alt="imgtwo"></img>*/}
+      <h1>Favorite your images <FontAwesomeIcon icon={faHeart} /></h1>
     </div>
   );
 };
